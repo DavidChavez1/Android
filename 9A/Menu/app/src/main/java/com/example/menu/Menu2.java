96e41e5828b8b7027c516eb8aa4d953f605de411
DavidChavez1 <davidchavez0103@gmail.com>
2019-09-10 16:27:31 -0500

    Cambios

--- 9A/Menu/app/src/main/java/com/example/menu/Menu2.java
+++ 9A/Menu/app/src/main/java/com/example/menu/Menu2.java
@@ -1,8 +1,13 @@
 package com.example.menu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Menu2 extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class Menu2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.colors2,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this, "Yellow", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.item2:
+                Toast.makeText(this, "Blue", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.item3:
+                Toast.makeText(this, "Red", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }// switch
+
     }
 }
