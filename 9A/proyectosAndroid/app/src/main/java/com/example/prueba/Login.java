a9b033bb0cf376cf7c16faf3e1c024304f80246a
DavidChavez1 <davidchavez0103@gmail.com>
2019-09-10 21:09:26 -0500

    Login Completo

--- 9A/proyectosAndroid/app/src/main/java/com/example/prueba/Login.java
+++ 9A/proyectosAndroid/app/src/main/java/com/example/prueba/Login.java
@@ -3,12 +3,33 @@ package com.example.prueba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    private EditText nombreU, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nombreU = findViewById(R.id.txtNomUsuario);
+        pass =  findViewById(R.id.txtPass);
+    }
+
+    public void Validar(View view){
+        if(nombreU.getText().toString().isEmpty() && pass.getText().toString().isEmpty()){
+            Toast.makeText(this, "Los campos estan vacios", Toast.LENGTH_SHORT).show();
+        } else if (nombreU.getText().toString().isEmpty()){
+            Toast.makeText(this, "Campo nombre usuario vacio", Toast.LENGTH_SHORT).show();
+        } else if (pass.getText().toString().isEmpty()){
+            Toast.makeText(this, "Campo password vacio", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Login correcto!!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- 9A/proyectosAndroid/app/src/main/java/com/example/prueba/MainActivity.java
+++ 9A/proyectosAndroid/app/src/main/java/com/example/prueba/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.prueba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText nombreU, pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombreU = findViewById(R.id.txtNomUsuario);
+        pass =  findViewById(R.id.txtPass);
+    }
+
+    private void Validar(View view){
+        if(nombreU.getText().toString().isEmpty() && pass.getText().toString().isEmpty()){
+            Toast.makeText(this, "Los campos estan vacios", Toast.LENGTH_SHORT).show();
+        } else if (nombreU.getText().toString().isEmpty()){
+            Toast.makeText(this, "Campo nombre usuario vacio", Toast.LENGTH_SHORT).show();
+        } else if (pass.getText().toString().isEmpty()){
+            Toast.makeText(this, "Campo password vacio", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Login correcto!!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
