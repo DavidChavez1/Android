4c7a0f759db006a3b75bda4e28833437820bca26
DavidChavez1 <davidchavez0103@gmail.com>
2019-09-10 22:12:48 -0500

    Login con cambio de actividad

--- 9A/proyectosAndroid/app/src/main/java/com/example/prueba/Login.java
+++ 9A/proyectosAndroid/app/src/main/java/com/example/prueba/Login.java
@@ -2,8 +2,10 @@ package com.example.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,6 +13,7 @@ import android.widget.Toast;
 public class Login extends AppCompatActivity {
 
     private EditText nombreU, pass;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,9 @@ public class Login extends AppCompatActivity {
 
         nombreU = findViewById(R.id.txtNomUsuario);
         pass =  findViewById(R.id.txtPass);
+
+        btn = findViewById(R.id.btnOpass);
+
     }
 
     public void Validar(View view){
@@ -32,4 +38,16 @@ public class Login extends AppCompatActivity {
             Toast.makeText(this, "Login correcto!!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void Pasar(View view){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+
 }
--- 9A/proyectosAndroid/app/src/main/java/com/example/prueba/MainActivity.java
+++ 9A/proyectosAndroid/app/src/main/java/com/example/prueba/MainActivity.java
@@ -2,32 +2,34 @@ package com.example.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText nombreU, pass;
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nombreU = findViewById(R.id.txtNomUsuario);
-        pass =  findViewById(R.id.txtPass);
+        btn = findViewById(R.id.btnOpass);
+
     }
 
-    private void Validar(View view){
-        if(nombreU.getText().toString().isEmpty() && pass.getText().toString().isEmpty()){
-            Toast.makeText(this, "Los campos estan vacios", Toast.LENGTH_SHORT).show();
-        } else if (nombreU.getText().toString().isEmpty()){
-            Toast.makeText(this, "Campo nombre usuario vacio", Toast.LENGTH_SHORT).show();
-        } else if (pass.getText().toString().isEmpty()){
-            Toast.makeText(this, "Campo password vacio", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Login correcto!!", Toast.LENGTH_SHORT).show();
-        }
+    public void Regresar(View view){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), Login.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
+
+
 }
