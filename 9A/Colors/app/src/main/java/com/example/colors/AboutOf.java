1796ecb930190d30c03fa5c49178f0d3cecc4aa2
DavidChavez1 <davidchavez0103@gmail.com>
2019-09-18 17:05:07 -0500

    Cambio de actividad

--- 9A/Colors/app/src/main/java/com/example/colors/AboutOf.java
+++ 9A/Colors/app/src/main/java/com/example/colors/AboutOf.java
@@ -2,12 +2,25 @@ package com.example.colors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutOf extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_of);
+
+        Button btn = findViewById(R.id.btnRegresar);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
