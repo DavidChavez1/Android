bc55fcfe65fec744c378e9df431c81405c67854b
DavidChavez1 <davidchavez0103@gmail.com>
2019-10-08 16:17:27 -0500

    Bd

--- 9A/conexionBD/app/src/main/java/com/example/conexionbd/clases/connectionBD.java
+++ 9A/conexionBD/app/src/main/java/com/example/conexionbd/clases/connectionBD.java
@@ -16,9 +16,12 @@ public class connectionBD extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase market) {
         //Crear las tablas de la bd
-        market.execSQL("create table users(id primary key not null," +
-                "firstname text not null, lastname text not null, " +
-                "email text not null, password text)");
+        market.execSQL("create table users(" +
+                "id primary key autoincrement not null," +
+                "firstname text not null, " +
+                "lastname text not null, " +
+                "email text not null, " +
+                "password text not null)");
     }
 
     @Override
